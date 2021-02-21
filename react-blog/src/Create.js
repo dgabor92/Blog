396d0297b0431687cd86b02a5745f9e160d6869a
Deutsch Gábor <deutschgabor92@gmail.com>
2021-02-21 20:23:53 +0100

    create the post req and the delete req

--- react-blog/src/Create.js
+++ react-blog/src/Create.js
@@ -1,14 +1,37 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom'; // import it to use for redirect
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory(); //this object represent History
+
+  //submit event
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+      // history.go(1);
+      history.push('/'); // THIS will redirect for the homepage
+    });
+  };
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      {/* added submit event to the form */}
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
@@ -28,7 +51,8 @@ const Create = () => {
           <option value="mario">Mario</option>
           <option value="yoshi">Yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Add Blog...</button>}
       </form>
     </div>
   );
