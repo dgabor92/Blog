139fe17c4f0c855b6e7b23527fbdb733ed0a213b
Deutsch Gábor <deutschgabor92@gmail.com>
2021-02-21 19:54:55 +0100

    controlled input forms, added css

--- react-blog/src/BlogDetails.js
+++ react-blog/src/BlogDetails.js
@@ -3,7 +3,7 @@ import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams(); //allow the grap Routes params from the route
-
+  //custom hook is able to create the BlogDetails component with fetch req
   const { data: blog, isPending, error } = useFetch(
     'http://localhost:8000/blogs/' + id
   );
--- react-blog/src/Create.js
+++ react-blog/src/Create.js
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)} //update the DOM every time type into the title field
+        />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
